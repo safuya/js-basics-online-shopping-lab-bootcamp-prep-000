9b21cfd3d9b8799c79116f5f422da0fb23ec4f70
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-13 11:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,22 +18,15 @@ function addToCart(item) {
 function viewCart() {
   let string
   if (cart.length < 1) {
-    string = 'Your shopping cart is empty.'
+    string = 'Your shopping cart is empty'
   } else {
-    let string = 'In your cart, you have'
-  }
-  
-  if (cart.length === 1) {
-    let key = Object.keys(cart[0])[0]
-    string += ` ${key} at $${cart[0][key]}`
-  }
-  
-  if (cart.length === 2) {
-    let string = 'In your cart, you have'
-    for (let cartIndex = 0; cartIndex < cart.length; cartIndex++) {
-      string += ` ${Object.keys(cart[cartIndex])[0]}`
+    string = 'In your cart, you have'
+    if (cart.length === 1) {
+      let key = Object.keys(cart[0])[0]
+      string += ` ${key} at $${cart[0][key]}`
     }
   }
+  
   
 }
 
